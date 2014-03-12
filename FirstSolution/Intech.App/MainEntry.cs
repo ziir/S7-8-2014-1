4ce7aa3c44e14e70a1ca65ad19f8a5d120840c99
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-03-12 09:36:40 +0100

    First commit.

---
+++ FirstSolution/Intech.App/MainEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intech.Business;
+
+namespace Intech.App
+{
+    class MainEntry
+    {
+        static void Main( string[] args )
+        {
+            FileProcessor p = new FileProcessor();
+            var r = p.Process( "C:\\Temp" );
+            Console.WriteLine( "TotalFileCount = {0}", r.TotalFileCount );
+            Console.WriteLine( "TotalDirectoryCount = {0}", r.TotalDirectoryCount );
+            Console.WriteLine( "HiddenFileCount = {0}", r.HiddenFileCount );
+            Console.WriteLine( "HiddenDirectoryCount = {0}", r.HiddenDirectoryCount );
+            Console.WriteLine( "There is {0} unaccessible file(s).", r.UnaccessibleFileCount );
+        }
+    }
+}
---
+++ FirstSolution/Intech.Business/FileProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intech.Business
+{
+    public class FileProcessor
+    {
+        public Result Process( string path )
+        {
+            return new Result();
+        }
+    }
+}
