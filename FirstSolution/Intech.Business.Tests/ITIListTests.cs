7c4c42495d99a8726bcbfae86205942a9f189ebc
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-03-13 17:57:02 +0100

    Start of ITIList<T>

---
+++ FirstSolution/Intech.Business.Tests/ITIListTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Intech.Business.Tests
+{
+    [TestFixture]
+    public class ITIListTests
+    {
+        [Test]
+        public void Basics()
+        {
+            ITIList<int> myList = new ITIList<int>();
+            ITIList<string> myList2 = new ITIList<string>();
+            var myList3 = new ITIList<ITIList<string>>();
+        }
+
+        [Test]
+        public void AddingItems()
+        {
+            // Arrange
+            ITIList<int> myList = new ITIList<int>();
+            Assert.That( myList.Count, Is.EqualTo( 0 ) );
+            // Act
+            myList.Add( 25 );
+            // Assert
+            Assert.That( myList.Count, Is.EqualTo( 1 ) );
+            Assert.That( myList[0], Is.InstanceOf( typeof( int ) ) );
+            Assert.That( myList[0], Is.EqualTo( 25 ) );
+            myList.RemoveAt( 0 );
+            Assert.That( myList.Count, Is.EqualTo( 0 ) );
+        }
+
+    }
+}
---
+++ FirstSolution/Intech.Business/ITIList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intech.Business
+{ 
+    public class ITIList<T>
+    {
+        T[] _array;
+        int _count;
+
+        public ITIList()
+        {
+            _array = new T[8];
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public void Add( T value )
+        {
+            if( _array.Length == _count )
+            {
+                var newOne = new T[ _array.Length * 2 ];
+                Array.Copy( _array, newOne, _array.Length );
+                _array = newOne;
+            }
+            _array[_count++] = value;
+        }
+
+        public T this[int i]
+        {
+            get 
+            {
+                if( i >= _count ) throw new IndexOutOfRangeException();
+                return _array[i]; 
+            }
+            set 
+            {
+                if( i >= _count ) throw new IndexOutOfRangeException();
+                _array[i] = value;
+            }
+        }
+
+        public void RemoveAt( int i )
+        {
+            if( i >= _count ) throw new IndexOutOfRangeException();
+            Array.Copy( _array, i + 1, _array, i, _count - (i+1) );
+            _array[--_count] = default( T );
+        }
+    }
+}
